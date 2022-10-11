baf0cbfd8fc9ff0fe0858ea60e947eaebc645bcf
Muver1337 <Muvers1s@gmail.com>
2022-10-12 00:53:14 +0700

    Initial commit

---
+++ locallibrary/catalog/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path(r'^books/$', views.BookListView.as_view(), name='books'),
+    path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+]
